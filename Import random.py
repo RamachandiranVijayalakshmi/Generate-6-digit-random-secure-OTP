1d43a132565159a2dad6d253477f446b43f799f0
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-08-18 22:31:58 +0530

    Add files via upload

---
+++ Import random.py
@@ -0,0 +1,9 @@
+import random
+
+print("Random integer from 0 to 9")
+num1 = random.randint(0, 9)
+print("Random integer: ", num1)
+
+print("Random integer from 10 to 100")
+num2 = random.randint(10, 100)
+print("Random integer: ", num2)
